5e4fc2d94beec2a0e1ea6406740586184ccd675f
hung <shalightdowdh@gmail.com>
2022-12-24 10:05:31 +0700

    ab

--- src/main/java/app/view/main/MainPage.java
+++ src/main/java/app/view/main/MainPage.java
@@ -11,6 +11,7 @@ import app.view.statistic.StatisticPage;
 import app.view.verify.VerifyPage;
 import com.jfoenix.controls.JFXButton;
 import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.input.MouseEvent;
@@ -27,13 +28,14 @@ public class MainPage extends Page implements Initializable {
     @FXML
     AnchorPane rootPane;
     @FXML
-    JFXButton homeButton,accountButton, registerButton, verifyButton, manageButton, statisticButton;
+    JFXButton homeButton,accountButton, registerButton, verifyButton, manageButton, statisticButton, settingButton,testButton;
     JFXButton [] buttons;
+    Page[] pages;
 
     public void showBigTaskBar(){
         if(!rootPane.getChildren().contains(bigTaskBar)) {
             rootPane.getChildren().add(bigTaskBar);
-            new FadeInLeft(bigTaskBar).play();
+            new FadeIn(bigTaskBar).play();
         }
     }
     public void unShowBigTaskBar(){
@@ -43,15 +45,6 @@ public class MainPage extends Page implements Initializable {
         System.out.println("abc");
     }
 
-
-    HomePage homePage = new HomePage();
-    AccountPage accountPage = new AccountPage();
-    RegisterPage registerPage = new RegisterPage();
-    VerifyPage verifyPage = new VerifyPage();
-    ManagePage managePage = new ManagePage();
-    StatisticPage statisticPage = new StatisticPage();
-
-    Page [] pages = {homePage, accountPage, registerPage, verifyPage, managePage, statisticPage};
 
     public int getSourceIndexButtons(MouseEvent mouseEvent){
         JFXButton button = (JFXButton) mouseEvent.getSource();
@@ -63,30 +56,40 @@ public class MainPage extends Page implements Initializable {
         return -1;
     }
 
-    public void showPage(Page page){
-        switchToPage(page, 75,0);
+    public void showPage(int index){
+        Page page = pages[index];
+        switchToPage(page, 100,120);
+        new FadeIn(page.getRoot()).play();
+        bigTaskBar.toFront();
     }
-
-    EventHandler<MouseEvent> buttonEventHandler = mouseEvent -> {
-        abc();
-        int index = getSourceIndexButtons(mouseEvent);
-        if(mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED){
-            System.out.println("deo hieu sao sai ak");
-            showPage(pages[index]);
-        }
-        if(mouseEvent.getEventType() == MouseEvent.MOUSE_ENTERED){
-            System.out.println("dsaf");
-        }
-    };
-
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         rootPane.getChildren().remove(bigTaskBar);
         bigTaskBar.setLayoutX(0);
-        buttons = new JFXButton[]{homeButton,accountButton, registerButton, verifyButton, manageButton, statisticButton};
+
+
+        HomePage homePage = new HomePage();
+        AccountPage accountPage = new AccountPage();
+        RegisterPage registerPage = new RegisterPage();
+        VerifyPage verifyPage = new VerifyPage();
+        ManagePage managePage = new ManagePage();
+        StatisticPage statisticPage = new StatisticPage();
+
+        buttons = new JFXButton[]{homeButton,accountButton, registerButton, verifyButton, manageButton, statisticButton,settingButton};
+        pages   =  new Page[]    {homePage  ,  accountPage, registerPage  , verifyPage  , managePage  , statisticPage  , statisticPage};
+
+        EventHandler<MouseEvent> buttonEventHandler = mouseEvent -> {
+            int index = getSourceIndexButtons(mouseEvent);
+            if(mouseEvent.getEventType() == MouseEvent.MOUSE_CLICKED){
+                if(pages[index]==null){
+                    System.out.println(pages[index].getClass().toString());
+                }
+                showPage(index);
+            }
+        };
         for(var button : buttons){
-            button.setOnMouseClicked(buttonEventHandler);
+            button.addEventHandler(MouseEvent.ANY,buttonEventHandler);
         }
     }
 }
