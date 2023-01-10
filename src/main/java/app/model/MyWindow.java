6046ba8f2c13176caf666fb5a5f48a07ae3b02ac
hung <shalightdowdh@gmail.com>
2023-01-10 20:31:01 +0700

    gần xong

--- src/main/java/app/model/MyWindow.java
+++ src/main/java/app/model/MyWindow.java
@@ -1,21 +1,23 @@
 package app.model;
 
 import animatefx.animation.*;
-import app.view.login.LoginPage;
 import javafx.application.Platform;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 import java.util.ArrayList;
+import java.util.concurrent.atomic.AtomicReference;
 
-public class Window extends Page{
+public class MyWindow extends Page{
+
     ArrayList<Page> children = new ArrayList<>();
-    public Window(){
-        super();
 
+    public MyWindow(){
+        super();
     }
     public void addWindow(Page window){
         children.add(window);
@@ -23,11 +25,25 @@ public class Window extends Page{
             Platform.runLater(()->{
                 Stage stage = new Stage();
                 stage.setResizable(false);
-                Scene scene = new Scene(window.getRoot());
+                Parent root = window.getRoot();
+                Scene scene = new Scene(root);
                 scene.setFill(Color.TRANSPARENT);
                 stage.setScene(scene);
                 stage.initStyle(StageStyle.TRANSPARENT);
                 stage.show();
+
+                AtomicReference<Double> xOffset = new AtomicReference<>((double) 0);
+                AtomicReference<Double> yOffset = new AtomicReference<>((double) 0);
+                root.setOnMousePressed((MouseEvent event) -> {
+                    xOffset.set(event.getSceneX());
+                    yOffset.set(event.getSceneY());
+                });
+
+                // Move around here
+                root.setOnMouseDragged((MouseEvent event) -> {
+                    stage.setX(event.getScreenX() - xOffset.get());
+                    stage.setY(event.getScreenY() - yOffset.get());
+                });
             });
         });
         thread.start();
@@ -53,4 +69,5 @@ public class Window extends Page{
         );
         thread.start();
     }
+
 }
